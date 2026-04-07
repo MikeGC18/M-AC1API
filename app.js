7c0879014d2f793adebd9e226109cb19cb2a9e05
MikeGC18 <mrobertgc@gmail.com>
2026-04-07 03:05:12 +0200

    Add slugify to welcome message

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const products_routes = require('./routes/products.js')
 //Server instantiation
 const app = express()
 
+const slugify = require('slugify');
+
 //Server configuration: template engine
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -15,7 +17,15 @@ app.use('/', products_routes)
 
 //Server startup
 app.listen(5000, () => {
-    console.log('server is listening on port 5000')
-})
+    const message = 'server is listening on port 5000';
+
+    const slugMessage = slugify(message, {
+        replacement: '*',
+        lower: false,
+        strict: true
+    });
+
+    console.log(slugMessage);
+});
 
 
